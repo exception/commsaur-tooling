7f3ea493565d35bf4911ab1b11f68a4674e88cd8
exception <erik@erosemberg.com>
2022-05-13 09:53:34 -0300

    enable domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['commsaur.mypinata.cloud'],
+        domains: ['commsaur.mypinata.cloud', 'd124myd65w4vuo.cloudfront.net'],
     },
 };
 
